1ba00153ca26539e57aaf62a9ab35cbae21ff219
Delta062004 <22bcs037@iiitdwd.ac.in>
2023-03-02 17:43:52 +0530

    determinant

--- 22BCS037_Lab6/problem_6_2.c
+++ 22BCS037_Lab6/problem_6_2.c
@@ -21,7 +21,7 @@ int main()
         printf("\n");
     }
     
-    int det=(arr[0][0])*((arr[1][1]*arr[2][2])-(arr[2][1]*arr[1][2]))-(arr[0][1])*((arr[1][0]*arr[2][2]))+(arr[0][2])*((arr[1][0]*arr[2][1])-(arr[2][0]*arr[1][1]));
+    int det=(arr[0][0])*((arr[1][1]*arr[2][2])-(arr[2][1]*arr[1][2]))-(arr[0][1])*((arr[1][0]*arr[2][2])-(arr[2][0]*arr[1][2]))+(arr[0][2])*((arr[1][0]*arr[2][1])-(arr[2][0]*arr[1][1]));
     printf("The Determinant is %d", det);
     return 0;
 }
