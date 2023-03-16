50f392b026238315f6c5c2bfb9841c1e3572cb98
Delta062004 <22bcs037@iiitdwd.ac.in>
2023-03-16 20:19:52 +0530

    modified

--- 22BCS037_Lab8/problem_8_4.c
+++ 22BCS037_Lab8/problem_8_4.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 #include<string.h>
-int palin(char str[100], int a);
+int palin(char str[], int a);
 int main()
 {
     char str[100];
     printf("Enter String\n");
     scanf("%s", str);
     int n= palin(str , 0);
-    n==0?printf("%s is a Palindrome\n", str) : printf("%s is not a Palindrome\n", str);
+    n==0?printf("Palindrome\n"):printf("Not a Palindrome\n");
 }
 
-int palin(char str[1000],int a)
+int palin(char str[],int a)
 {
-    int l=strlen(str);
-    if(a>=(l-1))
+    int length=strlen(str);
+    if(a>=(length-1))
     {
         return 0;
     }
-    if(str[a] != str[l-1])
+    if(str[a] != str[length-1])
     {
         return 1;
     }
-    str[l-1] = '\0';
+    str[length-1] = '\0';
     palin(str, a + 1);
 }
